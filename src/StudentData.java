bc68bb49b87775b91e8a5f696b1e5a52b4d212f3
AzharSailaubek <254664@astanait.edu.kz>
2026-01-09 18:07:31 +0500

    Initial commit

--- src/StudentData.java
+++ src/StudentData.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 
 public class StudentData {
 
-    public static ArrayList<UniversityStudent> getStudents() {
-        ArrayList<UniversityStudent> students = new ArrayList<>();
+    public static ArrayList<Student> getStudents() {
+        ArrayList<Student> students = new ArrayList<>();
 
-        students.add(new UniversityStudent("Ringo", "Starr", 2.3));
-        students.add(new UniversityStudent("Paul", "McCartney", 3.8));
+        students.add(new Student("Ringo", "Starr", 2.3));
+        students.add(new Student("Paul", "McCartney", 3.8));
 
         return students;
     }
